9a5af62f6bc22f39afa07392e6271e8b7ef7b9d1
hsnzync <hsnzync_@hotmail.com>
2023-12-24 11:56:43 +0100

    feat: move creds to env

--- packages/app/client.ts
+++ packages/app/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@sanity/client'
 
 export default createClient({
-    projectId: 'wdzh4l4i', // you can find this in sanity.json
-    dataset: 'production', // or the name you chose in step 1
-    useCdn: false, // `false` if you want to ensure fresh data
-    apiVersion: '2021-08-31', // use a UTC date string
+    projectId: process.env.NEXT_SANITY_PROJECT_ID,
+    dataset: process.env.NEXT_SANITY_DATASET,
+    useCdn: false,
+    apiVersion: '2021-08-31',
 })
